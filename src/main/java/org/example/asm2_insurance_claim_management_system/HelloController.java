1b924dec5ee487cd953137731310f6dd530bb2fe
ĐẠT <ungxuandat11092001@gmail.com>
2024-05-12 01:47:18 +0700

    create Login UI

--- src/main/java/org/example/asm2_insurance_claim_management_system/HelloController.java
+++ src/main/java/org/example/asm2_insurance_claim_management_system/HelloController.java
@@ -1,14 +1,62 @@
 package org.example.asm2_insurance_claim_management_system;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.example.asm2_insurance_claim_management_system.Admin.Admin;
+import org.example.asm2_insurance_claim_management_system.Login.Authentication;
+
+import java.io.IOException;
+import java.sql.Statement;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Button loginButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField textField;
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button createPolicyHolderButton;
+    @FXML
+    protected void onLoginButtonClick() {
+    String userName = textField.getText();
+    String password = passwordField.getText();
+    Authentication login = new Authentication();
+    Admin admin = Admin.getInstance();
+    if (login.authenticate(admin.listOfAdmin(), userName, password) != null){
+        System.out.println("Login successfully (Admin)");
+        try {
+            // Load the Admin.fxml file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Admin.fxml"));
+
+            // Load the root element (in this case, VBox)
+            VBox adminRoot = loader.load();
+
+            // Create a new stage for the Admin UI
+            Stage adminStage = new Stage();
+            adminStage.setTitle("Admin Page");
+            adminStage.setScene(new Scene(adminRoot, 320, 240));
+
+            // Show the Admin UI stage
+            adminStage.show();
+
+            // Close the current stage (the one containing the button)
+            loginButton.getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     }
 }
